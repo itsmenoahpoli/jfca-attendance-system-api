e4a94307eb7e6af4084127b5dcae53f65fbc0f47
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-10-07 08:10:31 +0800

    init

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'http://0.0.0.0:5173',
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.get('/api/healthcheck')
+def root():
+  return {
+    "status": "online",
+    "has_errors": True
+  }
