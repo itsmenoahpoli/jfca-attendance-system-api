b0a9b95357a11f3e0904af3db706eda10e7f12e6
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-11-02 20:37:49 +0800

    sms module - wip

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class AppSettings(BaseSettings):
-  app_debug: bool
+  app_debug: bool = False
   app_secret_key: str
   app_database_url: str
+  app_semaphore_url: str
+  app_semaphore_key: str
+  app_semaphore_sender_name: str
 
   class Config:
     env_file = '.env'
---
+++ app/libs/sms-lib.py
@@ -0,0 +1,36 @@
+import requests, urllib
+from config.settings import appSettings
+
+class SmsLib:
+    SEMAPHORE_SENDER_NAME=appSettings.app_semaphore_sender_name
+    SEMAPHORE_API_KEY=appSettings.app_semaphore_key
+    SEMAPHORE_API_URL=appSettings.app_semaphore_url
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def __createApiUrl(self, params: tuple):
+        return self.SEMAPHORE_API_URL + urllib.parse.urlencode(params)
+    
+    @classmethod
+    def __createParams(self, message: str, number: str):
+        return (
+                ('apikey', self.SEMAPHORE_API_KEY),
+                ('sendername', self.SEMAPHORE_SENDER_NAME),
+                ('message', message),
+                ('number', number)
+            )
+
+    @classmethod
+    def send_message(self, message: str, number: str):
+        try:
+            params = self.__createParams(message, number)
+            path = self.__createApiUrl(params)
+            response = requests.post(path)
+            response.raise_for_status()
+            
+            return True
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send message: {e}") 
+            return False  
